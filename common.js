5c0b1fdc00c520744cefeab418821550a8fba3d3
Saya <sayamatsui327@gmal.com>
2019-12-24 17:51:21 +0900

    send dataをcommon.jsに移動した

--- common.js
+++ common.js
@@ -2,5 +2,28 @@
 const urlSignUp = 'https://teachapi.herokuapp.com/sign_up';
 // [ユーザーログイン]
 const urlSignIn = 'https://teachapi.herokuapp.com/sign_in';
+//[ユーザー一覧]
+const urlUsers = 'https://teachapi.herokuapp.com/users';
 // [投稿一覧]
 const urlPosts = 'https://teachapi.herokuapp.com/posts';
+
+
+//MDNからコピペしてきた関数
+// function postData(url = ``, data = {}) {
+const sendData = (url = ``, data = {}, _method = "POST") => {
+  // 既定のオプションには * が付いています
+  return fetch(url, {
+    method: _method, // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, cors, *same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, same-origin, *omit
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      // "Content-Type": "application/x-www-form-urlencoded",
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+  })
+    .then(response => response.json()); // レスポンスの JSON を解析
+}
--- signUp.js
+++ signUp.js
@@ -1,25 +1,7 @@
 // []　配列
 // {} オブジェクト
 
-//MDNからコピペしてきた関数
-// function postData(url = ``, data = {}) {
-const sendData = (url = ``, data = {}, _method = "POST") => {
-  // 既定のオプションには * が付いています
-  return fetch(url, {
-    method: _method, // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, cors, *same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, same-origin, *omit
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      // "Content-Type": "application/x-www-form-urlencoded",
-    },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
-    body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
-  })
-    .then(response => response.json()); // レスポンスの JSON を解析
-}
+
 
 // サインアップを押されたらフェッチを実行するミドルウェア
 /**
@@ -43,6 +25,10 @@ const onButtonClickSignUp = () => {
       "password_confirmation": passwordConfirmation
     }
   }
+
+
+  console.log(urlSignUp, data)
+  
 
   // 登録APIを呼び出す
   sendData(urlSignUp, data).then(result => {

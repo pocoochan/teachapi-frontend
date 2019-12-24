49b8c0de3e3fd7fdc4e1080da24198e0105a9666
Saya <sayamatsui327@gmal.com>
2019-12-24 14:21:13 +0900

    汎用化した

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const urlSignIn =  'https://teachapi.herokuapp.com/sign_in';
 // [投稿一覧]
 const urlPosts =  'https://teachapi.herokuapp.com/posts';
 
-fetch
 
 // []　配列
 // {} オブジェクト
 
 //MDNからコピペしてきた関数
-function postData(url = ``, data = {}) {
+// function postData(url = ``, data = {}) {
+const sendData = (url = ``, data = {},  _method="POST") => {
   // 既定のオプションには * が付いています
     return fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        method: _method, // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, cors, *same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: "same-origin", // include, same-origin, *omit
@@ -28,7 +28,6 @@ function postData(url = ``, data = {}) {
     })
     .then(response => response.json()); // レスポンスの JSON を解析
 }
-
 
 
 //ばーちーさんのスラックより
@@ -45,7 +44,7 @@ const data = {
 console.log( 'start' )
 
 //使ってみる
-postData(urlSignUp, data)
+sendData(urlSignUp, data)
   .then(result => {
     console.log(result);
   })

34a1580b3a79ec7849abf5f934388961cd9bc8b4
Saya <sayamatsui327@gmal.com>
2019-12-24 17:09:48 +0900

    common.jsを作ってそこにapiURLを定義した

---
+++ common.js
@@ -0,0 +1,6 @@
+// [ユーザー登録]
+const urlSignUp = 'https://teachapi.herokuapp.com/sign_up';
+// [ユーザーログイン]
+const urlSignIn = 'https://teachapi.herokuapp.com/sign_in';
+// [投稿一覧]
+const urlPosts = 'https://teachapi.herokuapp.com/posts';
--- signUp.js
+++ signUp.js
@@ -1,11 +1,3 @@
-// [ユーザー登録]
-const urlSignUp = 'https://teachapi.herokuapp.com/sign_up';
-// [ユーザーログイン]
-const urlSignIn = 'https://teachapi.herokuapp.com/sign_in';
-// [投稿一覧]
-const urlPosts = 'https://teachapi.herokuapp.com/posts';
-
-
 // []　配列
 // {} オブジェクト
 

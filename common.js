9c6d2643d58bd2d85c3b0f5ad8e8c7e7271cf354
Saya <sayamatsui327@gmal.com>
2019-12-25 12:35:11 +0900

    GETメソッド用の関数とSignUp以外用関数（SignUpTokenチェックコードをheaderに追加）をcommon.jsに追加した！

--- common.js
+++ common.js
@@ -27,3 +27,38 @@ const sendData = (url = ``, data = {}, _method = "POST") => {
   })
     .then(response => response.json()); // レスポンスの JSON を解析
 }
+
+const sendGETData = (url = ``, data = {}, _method = "GET") => {
+  // 既定のオプションには * が付いています
+  return fetch(url, {
+    method: _method, // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, cors, *same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, same-origin, *omit
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": "Bearer " + localStorage.getItem("token")
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer",// no-referrer, *client
+  })
+    .then(response => response.json()); // レスポンスの JSON を解析
+}
+
+const sendDataWithToken = (url = ``, data = {}, _method = "POST") => {
+  // 既定のオプションには * が付いています
+  return fetch(url, {
+    method: _method, // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, cors, *same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, same-origin, *omit
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": "Bearer " + localStorage.getItem("token")
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+  })
+    .then(response => response.json()); // レスポンスの JSON を解析
+}

3e0ac6d9d027c23637ea5d50b05b841abba83215
Saya <sayamatsui327@gmal.com>
2019-12-25 14:51:29 +0900

    ログインができるようになった！

---
+++ signIn.js
@@ -0,0 +1,27 @@
+/**
+ * 登録ボタン押下時の処理
+ */
+const onButtonClickSignIn = () => {
+  // HTMLから値を取得する
+  const email = document.getElementById('signInEmail').value;
+  const password = document.getElementById('signInPassword').value;
+  const passwordConfirmation = document.getElementById('signInPasswordConfirmation').value;
+
+  //ばーちーさんのスラックより
+  const data = {
+    "sign_in_user_params": {
+      "email": email,
+      "password": password,
+      "passwordConfirmation": passwordConfirmation
+      }
+  }
+
+  console.log(urlSignIn, data)
+
+
+  // 登録APIを呼び出す
+  sendDataWithToken(urlSignIn, data).then(result => {
+    // ユーザー登録に成功して付与されたtokenをローカルストレージに保存する
+    localStorage.token = result.token;
+  });
+}
